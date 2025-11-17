bd59124880f975109d490f3cba5b3e5e2d4701d1
glorp-fr <63018960+glorp-fr@users.noreply.github.com>
2025-11-17 12:23:59 +0100

    debug output and rt

--- 05-terraform_oversec_output.tf
+++ 05-terraform_oversec_output.tf
@@ -4,14 +4,8 @@ output "ip_bastion" {
 }
 
 output "ip_sn2_vm1" {
-    value = outscale_vm.oversec_net2_sn1_vm1.private_ip
+    value = outscale_vm.oversec_net2_sn1_vm1.public_ip
 }
 
-output "ip_sn2_vm2" {
-    value = outscale_vm.oversec_net2_sn1_vm2.private_ip
-}
-output "ip_sn3_vm1" {
-    value = outscale_vm.oversec_net3_sn1_vm1.private_ip
-}
 
 
